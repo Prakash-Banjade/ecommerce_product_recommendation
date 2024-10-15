3310fef2f013ac4801742d907c55d6a9560656a7
Prakash Banjade <prakashbanjade191@gmail.com>
2024-10-15 20:43:06 +0545

    add suspense boundary

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,20 +1,14 @@
-import Header from "../components/header";
+import { Suspense } from "react";
 import HomeMainSection from "../components/homepage/main-section";
-import { trpc } from "./trpc"
+import { ProductsLoadingSkeleton } from "../components/product-skeleton";
 
 export default async function HomePage() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
-      <HomeMainSection />
-      <Footer />
+      <Suspense fallback={<ProductsLoadingSkeleton />}>
+        <HomeMainSection />
+      </Suspense>
     </div>
   )
-}
-
-function Footer() {
-  return <footer className="w-full py-4 text-center text-sm border-t">
-    © 2023 MinimalStore. All rights reserved.
-  </footer>
 }
